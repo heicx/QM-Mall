6345debb68d82069d419ed0bde00077bad5f0006
heicx <heicx_sudo@163.com>
2017-09-03 02:24:40 +0800

    feat: wallpaper ui fixed.

--- src/views/pr/WallPaper.vue
+++ src/views/pr/WallPaper.vue
@@ -1,14 +1,42 @@
 <template>
-	<div>
-    WallPaper
-	</div>
+	<div class="wallpaper-wrap">
+        <div class="wallpaper-head">
+            <button class='download'>壁纸下载</button>
+        </div>
+        <div class="wallpaper-content">
+            <ul class="img-list">
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+                <li>
+                    <img src="http://static.smartisanos.cn/common/pr/cover/jianguopro/tint-gold/7.png" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     methods: {},
-    components: {
-    },
     mounted() {
     },
     destroyed() {
@@ -17,4 +45,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wallpaper-wrap {
+    width: 1220px;
+    height: 100%;
+    overflow: hidden;    
+    margin: 0 auto;
+    background: #fff;
+    margin-top: 40px;
+    border-radius: 8px 8px 0 0;
+    padding-bottom: 40px;
+}
+
+.wallpaper-head {
+    width: 100%;
+    height: 60px;
+    border-bottom: 1px solid #d4d4d4;
+    border-radius: 8px 8px 0 0;
+    box-shadow: rgba(0,0,0,.06) 0 1px 7px;
+    background: #f3f3f3;
+    background: linear-gradient(#fbfbfb,#ececec);
+    .download {
+        float: right;
+        color: #666;
+        background: #ececec;
+        background: linear-gradient(#fff,#ececec);
+        width: 80px;
+        height: 38px;
+        line-height: 1;
+        border: 1px solid #dbdde2;
+        border-radius: 4px;
+        font-size: 14px;
+        margin: 12px 20px;
+        &:hover {
+            background: #e4e3e3;
+            background: linear-gradient(#fff,#e4e3e3);
+        }
+    }
+}
+
+.wallpaper-content .img-list{
+    margin-top: 30px;
+    padding-left: 30px;
+    li {
+        float: left;
+        width: 280px;
+        height: 189px;
+        border: 1px solid #e5e5e5;
+        margin: 0 10px 10px 0;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
 </style>
