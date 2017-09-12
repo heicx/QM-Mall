77a3d852f3ac2ba1986f177b37fda5ea98942b8b
heicx <heicx_sudo@163.com>
2017-09-12 17:06:31 +0800

    feat: signin & signup UI.

--- src/views/account/Signin.vue
+++ src/views/account/Signin.vue
@@ -16,7 +16,6 @@
 
 export default {
 	created() {
-		
 	},
 	mounted() {
 	},
@@ -25,50 +24,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-.signin-form {
-	width: 300px;
-	margin: 0 auto;
-}
-
-.group-inputs {
-	padding: 1px 0;
-	border: 1px solid #d5d5d5;
-	border-radius: 3px;
-	margin-top: 40px;
-	.input-wrapper {
-		font-size: 14px;
-		overflow: hidden;
-		input {
-			width: 100%;
-			border: 0;
-			border-radius: 0;
-			box-shadow: none;
-			background: #fff;
-			box-sizing: border-box;
-			padding: 1em .8em;
-			color: #555;
-		}
-		&+.input-wrapper {
-			border-top: 1px solid #d5d5d5;
-		}
-	}
-}
-
-.group-btn {
-	color: #fff;
-	background: #10afca;
-	width: 300px;
-	height: 40px;
-	line-height: 1;
-	border: 1px solid #6299b3;
-	border-radius: 6px;
-	font-size: 16px;
-	margin-bottom: 42px;
-	opacity: 0.75;
-	transition: opacity .15s ease-out;
-	&:hover {
-		opacity: 1;
-	}
-}
-</style>
+<style src='../../assets/style/scss/passportForm.scss' lang='scss'></style>
--- src/views/account/Signup.vue
+++ src/views/account/Signup.vue
@@ -1,10 +1,25 @@
 <template>
-	<div class="passport-form">
-	
+	<div class="signup-form">
+		<div class="group-inputs">
+			<div class="input-wrapper">
+				<input type="text" placeholder='手机号' maxlength='11' />
+			</div>
+			<div class="input-wrapper two-6-4">
+				<input type="text" cloumn='6' placeholder='短信验证码' maxlength='6' />
+				<button class='verification-code' cloumn='4'>获取验证码</button>
+			</div>
+			<div class="input-wrapper">
+				<input type="password" placeholder='密码' />
+			</div>
+			<div class="input-wrapper">
+				<input type="password" placeholder='重复密码' />
+			</div>
+		</div>
+		<button class="group-btn">登录</button>
 	</div>
 </template>
 
 <script>
 </script>
 
-<style lang="scss"></style>
+<style src='../../assets/style/scss/passportForm.scss' lang='scss'></style>
