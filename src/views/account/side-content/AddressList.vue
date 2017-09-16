9357bab2ee55c2c759bae3156f1a228cbab7d931
heicx <heicx_sudo@163.com>
2017-09-17 03:37:25 +0800

    feat: account-addressList UI.

--- src/views/account/side-content/AddressList.vue
+++ src/views/account/side-content/AddressList.vue
@@ -1,28 +1,132 @@
 <template>
   <div class='side-content'>
-    <p>尊敬的用户您好，感谢您选择我们的产品，如您在购买的过程中遇到任何疑问，请及时联系。</p>
-    <h3>购买渠道</h3>
-    <p>尊敬的用户您好，感谢您选择我们的产品，如您在购买的过程中遇到任何疑问，请及时联系。</p>
-    <h3>使用说明</h3>
-    <dl>
-      <dt>问：什么是电子优惠券？</dt>
-      <dd>
-        答：电子发票是指在购销商品、提供或者接受服务以及从事其他经营活动中，开具、收取的以电子方式存储的发票凭证，开具的电子发票均为真实有效的合法发票，与传统纸质发票具有同等法律效力，可作为用户报销、保修的有效凭证。
-      </dd>
-    </dl>
-    <dl>
-      <dt>问：什么是电子优惠券？</dt>
-      <dd>
-        答：电子发票是指在购销商品、提供或者接受服务以及从事其他经营活动中，开具、收取的以电子方式存储的发票凭证，开具的电子发票均为真实有效的合法发票，与传统纸质发票具有同等法律效力，可作为用户报销、保修的有效凭证。
-      </dd>
-    </dl>
-    <dl>
-      <dt>问：什么是电子优惠券？</dt>
-      <dd>
-        答：电子发票是指在购销商品、提供或者接受服务以及从事其他经营活动中，开具、收取的以电子方式存储的发票凭证，开具的电子发票均为真实有效的合法发票，与传统纸质发票具有同等法律效力，可作为用户报销、保修的有效凭证。
-      </dd>
-    </dl>
-    <h3>购买渠道</h3>
-    <p>尊敬的用户您好，感谢您选择我们的产品，如您在购买的过程中遇到任何疑问，请及时联系。</p>
+    <div class="address-group">
+      <div class="address-header">
+        <span class="column-name">姓名</span>
+        <span class="column-address">详细地址</span>
+        <span class="column-tel">电话</span>
+      </div>
+      <ul class="address-list">
+        <li class='list-item'>
+          <span class="name">张三</span>
+          <span class="address">北京市朝阳区宏泰东街绿地中心 A 座 A 区</span>
+          <span class="tel">
+            139****9088
+            <span class="default">(默认地址)</span>
+          </span>
+          <div class="operation">
+            <button class='edit'>编辑</button>
+            <button class='del'>删除</button>
+          </div>
+        </li>
+        <li class='list-item'>
+          <span class="name">李四</span>
+          <span class="address">北京市东城区东四十条佳汇中心 A 座</span>
+          <span class="tel">
+            186****2360
+          </span>
+          <div class="operation">
+            <button class='edit'>编辑</button>
+            <button class='del'>删除</button>
+          </div>
+        </li>
+      </ul>
+      <div class="address-add">
+        <button>添加收货地址</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.side-content {
+  margin-top: 0;
+}
+.address-header {
+  height: 38px;
+  line-height: 38px;
+  background: #EEE;
+  border-bottom: 1px solid #DBDBDB;
+  span {
+    display: inline-block;
+    font-size: 12px;
+    text-align: center;
+    color: #666;
+  }
+  .column-name {
+    padding-left: 27px;
+    width: 106px;
+    text-align: left;
+  }
+  .column-address {
+    width: 440px;
+    text-align: left;
+  }
+}
+.side-content-wrap .side-content li.list-item {
+  margin-top: 20px;
+  padding-bottom: 20px;
+  border-bottom: 1px dashed #EEE;
+}
+.list-item {
+  .name {
+    display: inline-block;
+    width: 106px;
+    text-align: left;
+  }
+  .address {
+    display: inline-block;
+    width: 440px;
+    text-align: left;
+  }
+  .tel {
+    display: inline-block;
+    width: 240px;
+    text-align: left;
+    .default {
+      padding-left: 34px;
+    }
+  }
+  .operation {
+    width: 120px;
+    display: inline-block;
+    vertical-align: middle;
+    text-align: right;
+    button {
+      width: 80px;
+      height: 30px;
+      border: 0;
+      border-radius: 4px;
+      background: #02bbda;
+      color: #fff;
+      font-size: 12px;
+      transition: opacity 0.15s ease-out;
+      opacity: .85;
+      &:hover {
+        opacity: 1;
+      }
+    }
+    .del {
+      margin-top: 12px;
+    }
+  }
+}
+.address-add {
+  margin: 40px 0 10px;
+  text-align: center;
+  button {
+    width: 180px;
+    height: 48px;
+    border: 0;
+    border-radius: 6px;
+    background: #02bbda;
+    color: #fff;
+    font-size: 14px;
+    transition: opacity 0.15s ease-out;
+    opacity: .85;
+    &:hover {
+      opacity: 1;
+    }
+  }
+}
+</style>
