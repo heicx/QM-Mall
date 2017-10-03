0bfe07f3451475ebf3b2f150814b5c8421036884
heicx <heicx_sudo@163.com>
2017-10-03 19:28:14 +0800

    fixed: navigation UI bugfix.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,26 +3,8 @@
     <div class="container clearfix">
       <h2 class="nav-series">P R O 1</h2>
       <ul class="nav-list">
-        <li>
-          <router-link to='/'>概览</router-link>
-        </li>
-        <li>
-          <router-link to='/'>设计</router-link>
-        </li>
-        <li>
-          <router-link to='/'>科技</router-link>
-        </li>
-        <li>
-          <router-link to='/'>参数</router-link>
-        </li>
-        <li>
-          <router-link to='/pr/video'>视频</router-link>
-        </li>
-        <li>
-          <router-link to='/'>用户评价</router-link>
-        </li>
-        <li>
-          <router-link to='/pr/wallpaper'>壁纸下载</router-link>
+        <li v-for='item in list' :class='{"active": item.link == $route.path}'>
+          <router-link :to='item.link'>{{item.name}}</router-link>
         </li>
       </ul>
       <button class="nav-btn">现在购买</button>
@@ -31,6 +13,45 @@
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          name: '概览',
+          link: '/'
+        },
+        {
+          name: '设计',
+          link: '/'
+        },
+        {
+          name: '科技',
+          link: '/'
+        },
+        {
+          name: '参数',
+          link: '/'
+        },
+        {
+          name: '视频',
+          link: '/pr/video'
+        },
+        {
+          name: '用户评价',
+          link: '/'
+        },
+        {
+          name: '壁纸下载',
+          link: '/pr/wallpaper'
+        }
+      ]
+    }
+  },
+  mounted() {
+    console.log(this.$route)
+  }
+}
 </script>
 
 <style lang="sass"></style>
